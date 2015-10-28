f19e8bc301f4c8f0d2b7c3032806c6becf8afe7f
Vocho <vocho@underdog.gs>
2015-10-28 14:02:07 -0600

    barra de titulo
    agrada parar de titulo con el video y elementos de fajardo

--- js/formula.js
+++ js/formula.js
@@ -6,46 +6,43 @@ $(document).ready(function(){
 	var client = new Sketchfab( version, iframe );
 	var showvideo = function(vcode){
 		"use strict";
-		$("#video_container").fadeIn("fast");
+		$("#model_video_display").fadeIn("fast");
 	  	$("#video").html('<iframe width="100%" height="100%"  src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&autospin=0.5" frameborder="0" allowfullscreen ></iframe>');
 
 	  	$("#close").on("click", function(){
-	  		$("#video_container").fadeOut("fast");
+	  		$("#model_video_display").fadeOut("fast");
 	  		$("#video").html("");
 	  	});
 	}; 
+
+
 
 	client.init( urlid, {
 		success: function onSuccess( api ){
 			//api.load();
 		    api.start(function(){
-		    	$(".opciones").each(function(){
-		    		$(this).click(function(){
-		    			api.gotoAnnotation( $(this).attr("opcion") );
-		    		});				    		
-		    	})
+		    	$(".infoblock").css({"display: none !important"});
 		    });
 		    api.addEventListener( 'viewerready', function() {
 		    	$("#annotations").fadeIn("slow");
 		    } );
 		    api.addEventListener( 'annotationFocus', function( index ) {
 			    //console.log( index );
-
 				api.getCameraLookAt( function( err, camera ){
 					var camtarget = camera.target;
 					var strtarget = camtarget.toString();
 					var strsmall = strtarget.substr(0, 7);
 
-					if( strsmall == 1.65842){
-						//showvideo("Table dance");
-					}else if( strsmall == 1.69556){
-						//showvideo("OXXO");
-					}else if( strsmall == -2.8221){
+					if( strsmall == -1.0880){
 						showvideo("CzskNOQ0AbU");
-					}else if( strsmall == 0.46786){
-						//showvideo("Ticketmaster");
-					}else if( strsmall == -2.8130){
-						//showvideo("Comercial Mexicana");
+					}else if( strsmall == -3.0378){
+						showvideo("CzskNOQ0AbU");
+					}else if( strsmall == -3.3181){
+						showvideo("CzskNOQ0AbU");
+					}else if( strsmall == 1.83145){
+						showvideo("CzskNOQ0AbU");
+					}else if( strsmall == -0.2837){
+						showvideo("CzskNOQ0AbU");
 					}
 				});
 			} );
